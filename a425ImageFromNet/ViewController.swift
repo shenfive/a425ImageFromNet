ddb4f404ae8d53e10bdf0aef40d77152f0179146
Shen Jun Wu <shenfive@gmail.com>
2021-05-09 17:12:47 +0800

    background

--- a425ImageFromNet/ViewController.swift
+++ a425ImageFromNet/ViewController.swift
@@ -12,18 +12,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("1:\(Date().timeIntervalSince1970)")
         let urlString = "https://lh3.googleusercontent.com/proxy/7N25TmqlhnQvdpklusX3SmyRC1MKL3eUkmPMo-51Bz19qk4lmkHLqEX10BxKu6Z7pI07COtNuBkCGdrLvzYnhaSByUJeHdU"
         let url = URL(string: urlString)!
-        
-        
-        
-        do {
-            let imageData = try Data(contentsOf: url)
-            theImageView.image = UIImage(data: imageData)
-        } catch {
-            print(error.localizedDescription)
+        DispatchQueue.global().async {
+            do {
+                let imageData = try Data(contentsOf: url)
+                
+                DispatchQueue.main.async {
+                    self.theImageView.image = UIImage(data: imageData)
+                    print("2:\(Date().timeIntervalSince1970)")
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
         }
         
+        print("3:\(Date().timeIntervalSince1970)")
         
         
     }
