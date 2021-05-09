c9440a8668ee935b3c0b6bb9c13f7ad426be1591
Shen Jun Wu <shenfive@gmail.com>
2021-05-09 16:55:05 +0800

    image

--- a425ImageFromNet/ViewController.swift
+++ a425ImageFromNet/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let urlString = "https://lh3.googleusercontent.com/proxy/7N25TmqlhnQvdpklusX3SmyRC1MKL3eUkmPMo-51Bz19qk4lmkHLqEX10BxKu6Z7pI07COtNuBkCGdrLvzYnhaSByUJeHdU"
+        let url = URL(string: urlString)!
+        
+        
+        
+        do {
+            let imageData = try Data(contentsOf: url)
+            theImageView.image = UIImage(data: imageData)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        
     }
 
 
